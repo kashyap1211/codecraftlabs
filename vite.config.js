1a6e90f41ac2fd37568592f785bc9792f8f4a211
kashyap1211 <kashyappatel1211@gmail.com>
2026-04-29 13:55:36 +0530

    remove manualChunks and fix build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+// // import tailwindcss from '@tailwindcss/vite'
+
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react(), tailwindcss()],
+// //   build: {
+// //     target: 'esnext',
+// //     minify: 'terser',
+// //     terserOptions: {
+// //       compress: {
+// //         drop_console: true,
+// //         drop_debugger: true,
+// //       },
+// //     },
+// //     rollupOptions: {
+// //       output: {
+// //         manualChunks: {
+// //           vendor: ['react', 'react-dom', 'react-router-dom'],
+// //           animations: ['framer-motion'],
+// //           ui: ['lucide-react', 'react-icons'],
+// //         },
+// //       },
+// //     },
+// //     chunkSizeWarningLimit: 1000,
+// //   },
+// // })
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
 
-
-// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react(), tailwindcss()],
 //   build: {
@@ -17,16 +44,20 @@
 //     },
 //     rollupOptions: {
 //       output: {
-//         manualChunks: {
-//           vendor: ['react', 'react-dom', 'react-router-dom'],
-//           animations: ['framer-motion'],
-//           ui: ['lucide-react', 'react-icons'],
-//         },
-//       },
+//         manualChunks(id) {
+//           if (id.includes('node_modules')) {
+//             if (id.includes('framer-motion')) return 'animations'
+//             if (id.includes('lucide-react') || id.includes('react-icons')) return 'ui'
+//             return 'vendor'
+//           }
+//         }
+//       }
 //     },
 //     chunkSizeWarningLimit: 1000,
 //   },
 // })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -35,24 +66,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     target: 'esnext',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('framer-motion')) return 'animations'
-            if (id.includes('lucide-react') || id.includes('react-icons')) return 'ui'
-            return 'vendor'
-          }
-        }
-      }
-    },
+    // minify भी अभी हटा दो ताकि terser issue ना आए
     chunkSizeWarningLimit: 1000,
   },
 })
