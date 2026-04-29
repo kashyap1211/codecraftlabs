d9a9af01238dedc839a8220d97da0228d20f9f5b
kashyap1211 <kashyappatel1211@gmail.com>
2026-04-29 14:16:36 +0530

    remove manualChunks

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,9 @@
-// // import { defineConfig } from 'vite'
-// // import react from '@vitejs/plugin-react'
-// // import tailwindcss from '@tailwindcss/vite'
-
-
-// // // https://vite.dev/config/
-// // export default defineConfig({
-// //   plugins: [react(), tailwindcss()],
-// //   build: {
-// //     target: 'esnext',
-// //     minify: 'terser',
-// //     terserOptions: {
-// //       compress: {
-// //         drop_console: true,
-// //         drop_debugger: true,
-// //       },
-// //     },
-// //     rollupOptions: {
-// //       output: {
-// //         manualChunks: {
-// //           vendor: ['react', 'react-dom', 'react-router-dom'],
-// //           animations: ['framer-motion'],
-// //           ui: ['lucide-react', 'react-icons'],
-// //         },
-// //       },
-// //     },
-// //     chunkSizeWarningLimit: 1000,
-// //   },
-// // })
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 // import tailwindcss from '@tailwindcss/vite'
 
+
+// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react(), tailwindcss()],
 //   build: {
@@ -44,18 +17,17 @@
 //     },
 //     rollupOptions: {
 //       output: {
-//         manualChunks(id) {
-//           if (id.includes('node_modules')) {
-//             if (id.includes('framer-motion')) return 'animations'
-//             if (id.includes('lucide-react') || id.includes('react-icons')) return 'ui'
-//             return 'vendor'
-//           }
-//         }
-//       }
+//         manualChunks: {
+//           vendor: ['react', 'react-dom', 'react-router-dom'],
+//           animations: ['framer-motion'],
+//           ui: ['lucide-react', 'react-icons'],
+//         },
+//       },
 //     },
 //     chunkSizeWarningLimit: 1000,
 //   },
 // })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -64,6 +36,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     target: 'esnext',
+    // terser भी हटाना safe है (optional)
     chunkSizeWarningLimit: 1000,
   },
 })
